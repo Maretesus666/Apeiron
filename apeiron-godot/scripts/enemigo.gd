287e9e6448ec2309ca7aa1377205b90115e7fc8e
Maretesus666 <frnclmb@gmail.com>
2026-01-11 16:07:21 -0300

    hub cambios

--- apeiron-godot/scripts/enemigo.gd
+++ apeiron-godot/scripts/enemigo.gd
@@ -1,9 +1,9 @@
 extends Area2D
 
 @export var acceleration: float = 400.0
-@export var max_speed: float = 2000.0
-@export var friction: float = 100.0
-@export var rotation_speed: float = 50.0
+@export var max_speed: float = 800.0
+@export var friction: float = 0.0
+@export var rotation_speed: float =  5.0
 @export var health = 3
 @export var damage = 1
 
